894494bdf7b18e804c92b1df8296a23783c1646b
Javier-Fera <javialofer@hotmail.com>
2020-08-24 15:37:10 -0600

    html skeleton

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 24/8/20
 
 Project Begins!
+
+Planned Workflow:
+
+1. Make a skeleton HTML for each page, mobile first.
+2. JS Functions
+3. CSS styling for mobile view first.
+4. Responsive layout
+5. Launch project in Heroku.
+
+24/8/20
+
+2:40 pm: created skeleton HTML 
---
+++ script/script.js
@@ -0,0 +1,21 @@
+//Required functions
+
+//fetch function
+
+//build response function with success and failure responses
+
+//regex email check with border change
+
+var email;
+
+function storedata(a){
+    email=a;
+}
+
+function writedata(){
+    document.getElementById("email_result").innerHTML=email;
+}
+
+function launchAlert(){
+    alert("message");
+}
