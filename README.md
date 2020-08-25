8de7463296cb759be9fc2e9125758dad35c191f6
Javier-Fera <javialofer@hotmail.com>
2020-08-24 20:10:26 -0600

    results will read the JSON values from fetch

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Web Challenge Project
 
-24/8/20
+###24/8/20
 
 Project Begins!
 
@@ -12,6 +12,21 @@ Planned Workflow:
 4. Responsive layout
 5. Launch project in Heroku.
 
-24/8/20
+Today's Progress: 
 
-2:40 pm: created skeleton HTML 
+Created skeleton HTML 
+The result page will get a JSON from a fetch. If the fetch is successful it will arrange the values as shown in the mock up. If nothing is returned then a message will appear stating the search yielded no results.
+
+Next up:
+
+Passing a value from one page to the other as a variable to call the fetch.
+Functionallity to the buttons.
+A (very likely regex) function to avoid searching for queries that does not fit the text@text.com structure or empty values.
+
+After that:
+Begin with styling in mobile
+Add responsiveness
+
+Finally:
+Study and launch on Heroku.
+
--- script/script.js
+++ script/script.js
@@ -6,16 +6,50 @@
 
 //regex email check with border change
 
-var email;
+async function writedata() {
 
-function storedata(a){
-    email=a;
+    var results;
+    fetch("https://ltv-data-api.herokuapp.com/api/v1/records.json?email=garciansmith@example.com")
+        .then(res => res.json())
+        .then(data => results = data)
+        .then(() => {
+            if (JSON.stringify(results) == "[]") {
+                notFoundMessageDisplay();
+            }
+            else {
+                document.getElementById("result_name").innerHTML = JSON.stringify(results.first_name);
+                document.getElementById("result_description").innerHTML = JSON.stringify(results.description);
+                document.getElementById("result_address").innerHTML = JSON.stringify(results.address);
+                document.getElementById("result_email").innerHTML = JSON.stringify(results.email);
+
+                Object.values(results.phone_numbers).forEach(element => {
+                    document.getElementById("result_phone")
+                        .innerHTML += "<p>" + element + "</p>"
+                });
+
+                Object.values(results.relatives).forEach(element => {
+                    document.getElementById("result_relatives")
+                        .innerHTML += "<p>" + element + "</p>"
+                });
+            }
+        })
 }
 
-function writedata(){
-    document.getElementById("email_result").innerHTML=email;
+function notFoundMessageDisplay() {
+    document.getElementById("results_view").innerHTML =
+        "<div><h2>Nothing was Found</h2>"
+        + "<p class='r_description'>The search you made yielded no results...</p></div>";
 }
 
-function launchAlert(){
-    alert("message");
+function testing() {
+    var results;
+    fetch("https://ltv-data-api.herokuapp.com/api/v1/records.json?email=doesith@example.com")
+        .then(res => res.json())
+        .then(data => results = data)
+        .then(() => {
+            console.log("breakdown");
+            if (JSON.stringify(results) == "[]") {
+                console.log("There's Nothing here");
+            }
+        });
 }
